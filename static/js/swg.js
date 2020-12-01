44abef0155e7d3f9c569d0daef71cc62992b6462
vignesh-cloud-prog <vigneshun80@gmail.com>
2020-12-01 20:02:35 +0530

    game improved
    Signed-off-by: vignesh-cloud-prog <vigneshun80@gmail.com>

--- static/js/swg.js
+++ static/js/swg.js
@@ -19,17 +19,19 @@ function copyFunc() {
 function sign() {
     let regex = /[a-zA-Z]{3,15}/;
     let reg = /[5-50]/;
-    namesign = ` <div  style=" position: absolute; top: 20%;  width: 100%; height: 100%;">
-    <div  style="width: 90%; height:60%; background: linear-gradient(to bottom, #ff0000 0%, #6666ff 100%); border-radius: 28px; border: 6px solid rgb(255, 196, 0); margin: auto;display: flex;flex-direction: column;justify-content: center;">
+    namesign = ` <div id="abs"  style=" position: absolute; top: 20%;  width: 100vh;max-width:1000px; height: 100vh;">
+    <div  style="width: 90%; height:60%; background: linear-gradient(to bottom, #ff0000 0%, #6666ff 100%); border-radius: 28px; border: 6px solid rgb(255, 196, 0); margin: auto;display: flex;flex-direction: column;justify-content: center;align-self:center;">
     <p style="text-align: center; width:100%; margin:10vh 0 5vh 0; font-size: 4vh;font-weight: bold; background-image: linear-gradient(to right, red, green 33%,blue 35%, black);
     color: transparent;
     -webkit-background-clip: text;
     background-clip: text;">Snake Water Gun</p>
-    <lable style=" font-size:3vh; margin:0 8vh;">Your Name : </lable>
-    <input id="name" type="text" placeholder="Enter your name " style="font-weight:bold;text-align:center; border-radius: 30px; width: 60%;height:7%; margin:1vh 8vh; padding:5px; border: 4px solid rgba(255, 128, 43, 0.849); font-family:monospace;font-size:2vh;">
-    <lable style=" font-size:2vh; margin:0 8vh;margin-top:2vh">How many Rounds <br> <small>do you want to play?</small> </lable>
-    <input id="noOfRounds" type="number" min="5" max="50" step="5" value="10" style="font-weight:bold;text-align:center; border-radius: 30px; width: 60%;height:6%; margin:1vh 8vh; padding:5px; border: 4px solid rgba(255, 128, 43, 0.849); font-family:monospace;font-size:2vh;">
-    <button id="play" style=" border-radius: 23px; width: 50%; margin: 4vh auto;margin-bottom:10vh;  background: linear-gradient(to bottom, #0066ff 0%, #ff99cc 100%); padding:1.5vh; border: 5px solid rgb(72, 72, 231); font-size: 4vh; font-family: 'Fredoka One', cursive;">Play</button>
+    <div style = "margin:auto;">
+    <lable style=" font-size:3vh; margin:0 8vh;display:block;">Your Name : </lable>
+    <input id="name" type="text" placeholder="Enter your name " style="font-weight:bold;text-align:center; border-radius: 30px; width: 60%;height:7%; margin:1vh 8vh; padding:1em; border: 4px solid rgba(255, 128, 43, 0.849); font-family:monospace;font-size:2vh;">
+    <lable style="display:block; font-size:2vh; margin:0 8vh;margin-top:2vh">How many Rounds <br> <small>do you want to play?</small> </lable>
+    <input id="noOfRounds" type="number" min="5" max="50" step="5" value="10" style="font-weight:bold;text-align:center; border-radius: 30px; width: 60%;height:6%; margin:1vh 8vh; padding:1em;  border: 4px solid rgba(255, 128, 43, 0.849); font-family:monospace;font-size:2vh;">
+    </div>
+    <button id="play" style=" border-radius: 23px; width: 15rem; margin: 4vh auto;margin-bottom:10vh;  background: linear-gradient(to bottom, #0066ff 0%, #ff99cc 100%); padding:1.5vh; border: 5px solid rgb(72, 72, 231); font-size: 4vh; font-family: 'Fredoka One', cursive;">Play</button>
     </div> </div>`
 
     document.getElementById("entry").innerHTML = namesign
